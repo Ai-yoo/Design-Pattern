19300ce841b043ca9118f4e9cd012e61231ce40c
Ai-yoo <duzhentong123@foxmail.com>
2018-01-29 10:21:55 +0800

    具体处理者2

---
+++ chain/demo_zeren/SuperStudentTwo.java
@@ -0,0 +1,28 @@
+package demo_zeren;
+
+/**
+ * Created with IDEA
+ * author:DuzhenTong
+ * Date:2017/11/14
+ * Time:18:53
+ */
+public class SuperStudentTwo extends Handler {
+    //结果
+    String result = "";
+
+    //处理方法
+    @Override
+    public String result(int different) {
+        if (different <= 8) {
+            result = "学霸2处理了难度系数为" + different + "的问题!";
+        } else {
+            //判断后面是否还有处理者，有交给后面，没有返回
+            if (getHandler() != null) {
+                return getHandler().result(different);
+            } else {
+                result = "问题太难了!";
+            }
+        }
+        return result;
+    }
+}
