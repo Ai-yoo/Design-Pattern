b5e2122d1679b697a0bd836a70bcd859be11d86b
Ai-yoo <duzhentong123@foxmail.com>
2018-02-07 11:56:26 +0800

    方式一代码

---
+++ template/demo_template/First.java
@@ -0,0 +1,19 @@
+package demo_template;
+
+public class First extends Template {
+
+    @Override
+    public void selectA() {
+        System.out.println("地三鲜");
+    }
+
+    @Override
+    public void selectB() {
+        System.out.println("大米饭");
+    }
+
+    @Override
+    public void selectC() {
+        System.out.println("橙汁");
+    }
+}
