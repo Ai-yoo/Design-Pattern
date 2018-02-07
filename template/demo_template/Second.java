3c6dae60185f0dcd1f9387f17f360350031d2157
Ai-yoo <duzhentong123@foxmail.com>
2018-02-07 11:56:50 +0800

    方式二代码

---
+++ template/demo_template/Second.java
@@ -0,0 +1,18 @@
+package demo_template;
+
+public class Second extends Template {
+    @Override
+    public void selectA() {
+        System.out.println("大米饭");
+    }
+
+    @Override
+    public void selectB() {
+        System.out.println("糖醋鲤鱼");
+    }
+
+    @Override
+    public void selectC() {
+        System.out.println("地三鲜");
+    }
+}
